8fcab4bf6d1e66c2ae7e440b795f8be200599814
Victoria LeBel <vickileb@hotmail.com>
2022-08-08 22:12:14 -0700

    commit tree e16563cf2fc81e58a14ab30bc691e418abbc787a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,13 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students.select(params[{first_name || last_name}])
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
